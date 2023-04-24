ac49b86390c648524d4ac9a2a0ae52623fd3354d
Anastasya <anasty22@gmail.com>
2023-04-24 16:00:32 +0300

    add modal

---
+++ js/buttons.js
@@ -0,0 +1,14 @@
+
+
+	// функция для показа модального окна
+	function showModal(modalId) {
+		var modal = document.getElementById(modalId);
+		modal.style.display = "block";
+	}
+
+	// функция для скрытия модального окна
+	function hideModal(modalId) {
+		var modal = document.getElementById(modalId);
+		modal.style.display = "none";
+	}
+
